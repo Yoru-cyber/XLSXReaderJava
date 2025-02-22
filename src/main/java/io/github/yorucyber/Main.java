b94758e0354d8924e548f86b94a772a9b53d789e
Yoru-cyber <carlosmendez170210@gmail.com>
2025-02-22 17:13:01 -0400

    added currentSheet attribute to XLSXReader to be able to change and see the used sheet, added excelFile property which is an instance of ZipFile provided by user. Added Setter and Getter for currentSheet

--- src/main/java/io/github/yorucyber/Main.java
+++ src/main/java/io/github/yorucyber/Main.java
@@ -1,18 +1,28 @@
 package io.github.yorucyber;
 
 import io.github.yorucyber.XLSXReader.XLSXReader;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.zip.ZipFile;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException {
         String xlsxFilePath = "./FinancialSample.xlsx";
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        XLSXReader xlsxReader = new XLSXReader(documentBuilder);
-        xlsxReader.GetEntries(xlsxFilePath);
-//        var dirs = XLSXReader.GetEntries(zipFilePath);
+        try (ZipFile excelFile = new ZipFile(xlsxFilePath)) {
+            XLSXReader xlsxReader = new XLSXReader(documentBuilder, excelFile);
+            xlsxReader.SetCurrentSheet("xl/worksheets/sheet1.xml");
+            Document doc = xlsxReader.GetCurrentSheet();
+        } catch (IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
--- src/main/java/io/github/yorucyber/XLSXReader/XLSXReader.java
+++ src/main/java/io/github/yorucyber/XLSXReader/XLSXReader.java
@@ -11,26 +11,45 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class XLSXReader {
-    private final DocumentBuilder documentBuilder;
+    private DocumentBuilder documentBuilder = null;
+    private Document currentSheet = null;
+    private ZipFile excelFile = null;
 
-    public XLSXReader(DocumentBuilder documentBuilder) {
+    public XLSXReader(DocumentBuilder documentBuilder, ZipFile excelFile) {
         this.documentBuilder = documentBuilder;
+        this.excelFile = excelFile;
+
+
     }
 
-    public void GetEntries(String filePath) {
-        try (ZipFile zipFile = new ZipFile(filePath)) {
-            Enumeration<? extends ZipEntry> entries = zipFile.entries();
-            while (entries.hasMoreElements()) {
-                ZipEntry entry = entries.nextElement();
-                String entryName = entry.getName();
-                if (entryName.endsWith(".xml") && entryName.startsWith("xl/")) {
-                    InputStream inputStream = zipFile.getInputStream(entry);
+    public Document GetCurrentSheet() {
+        return this.currentSheet;
+    }
+
+    public void SetCurrentSheet(String sheetName) throws IOException, SAXException {
+        ZipEntry sheet = this.excelFile.getEntry(sheetName);
+        InputStream sheetStream = this.excelFile.getInputStream(sheet);
+        this.currentSheet = this.documentBuilder.parse(sheetStream);
+    }
+
+//    public void PrintCell() {
+//
+//    }
+
+    public void GetEntries() {
+        Enumeration<? extends ZipEntry> entries = this.excelFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            String entryName = entry.getName();
+            if (entryName.endsWith(".xml") && entryName.startsWith("xl/")) {
+                InputStream inputStream = null;
+                try {
+                    inputStream = this.excelFile.getInputStream(entry);
                     Document document = this.documentBuilder.parse(inputStream);
-                    document.getDocumentElement().normalize();
+                } catch (IOException | SAXException e) {
+                    throw new RuntimeException(e);
                 }
             }
-        } catch (IOException | SAXException e) {
-            throw new RuntimeException(e);
         }
     }
 }
